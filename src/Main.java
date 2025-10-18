064e8cf4098c1605e59e46dd01517b0fb62ff7d7
biancaholobica <bianca.ioana.holobica@stud.ubbcluj.ro>
2025-10-18 19:01:27 +0300

    getMaxSum, getMinSum

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+    List<Integer> inputNumbers = new ArrayList<>();
+    inputNumbers.add(29);
+    inputNumbers.add(37);
+    inputNumbers.add(38);
+    inputNumbers.add(41);
+    inputNumbers.add(84);
+    inputNumbers.add(67);
 
+    Numbers numbers = new Numbers(inputNumbers);
+
+    System.out.println("Max number: " + numbers.getMaxNumber());
+    System.out.println("Min number: " + numbers.getMinNumber());
+    System.out.println("Max sum of n-1 numbers: " + numbers.getMaxSum());
+    System.out.println("Min sum of n-1 numbers: " + numbers.getMinSum());
 }
